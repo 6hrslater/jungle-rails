16f27f458d74edc2ed2c39217ad810f0d01b64fa
Darian Chen <47340623+6hrslater@users.noreply.github.com>
2022-08-30 02:42:09 -0700

    Add admin authentication WIP

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,23 +1,20 @@
 class Admin::ProductsController < ApplicationController
+  before_action :authenticate
 
   def index
     @products = Product.order(id: :desc).all
   end
-
   def new
     @product = Product.new
   end
-
   def create
     @product = Product.new(product_params)
-
     if @product.save
       redirect_to [:admin, :products], notice: 'Product created!'
     else
       render :new
     end
   end
-
   def destroy
     @product = Product.find params[:id]
     @product.destroy
@@ -25,7 +22,6 @@ class Admin::ProductsController < ApplicationController
   end
 
   private
-
   def product_params
     params.require(:product).permit(
       :name,
@@ -36,5 +32,10 @@ class Admin::ProductsController < ApplicationController
       :price
     )
   end
-
+  protected
+    def authenticate
+        authenticate_or_request_with_http_basic do |username, password|
+        username == ENV['USERNAME'] && password == ENV['PASSWORD']
+      end
+    end
 end
