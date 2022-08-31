8795ba9c5a1f03f05a79a381f86699426089d094
Darian Chen <47340623+6hrslater@users.noreply.github.com>
2022-08-30 23:51:28 -0700

    Add user authentication WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'authentication/register'
+  get 'authentication/signin'
   get 'about/index'
   root to: 'products#index'
 
@@ -15,7 +17,8 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'dashboard#show'
-    resources :products, except: [:edit, :update, :show]
+    resources :products, except: [:edit, :update, :show] 
+    resources :categories, except: [:edit, :update, :show, :delete]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
