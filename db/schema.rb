8d44e10687442c754030e1b2f81aa7d752a8f55f
Darian Chen <47340623+6hrslater@users.noreply.github.com>
2022-09-28 16:57:53 -0700

    Update admin auth and fixed broken migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_28_083001) do
+ActiveRecord::Schema.define(version: 2022_07_29_000624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 2022_08_28_083001) do
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "line_items", "orders"
